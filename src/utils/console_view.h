e61af607e5a134ddf8c2d59cfad9b3bf849c065a
Artur Huletski <hatless.fox@gmail.com>
2017-04-06 17:59:55 +0300

    [Fmwk] Console map view refactoring

--- src/utils/console_view.h
+++ src/utils/console_view.h
@@ -2,13 +2,15 @@
 #define SLAM_CTOR_UTILS_CONSOLE_VIEW_H_INCLUDED
 
 void show_grid_map(const GridMap &map, const Point2D &center,
-                   int d_x_left, int d_x_right, int d_y_up, int d_y_down) {
+                   double d_x_left, double d_x_right,
+                   double d_y_up, double d_y_down) {
   auto center_cell = map.world_to_cell(center);
-  auto sc = map.scale();
 
   auto delta_c = DiscretePoint2D{0, 0};
-  for (delta_c.y = d_y_up / sc; -d_y_down / sc <= delta_c.y; --delta_c.y) {
-    for (delta_c.x = -d_x_left / sc; delta_c.x <= d_x_right / sc; ++delta_c.x) {
+  auto top_left = map.world_to_cell(d_x_left, d_y_up);
+  auto bot_right = map.world_to_cell(d_x_right, d_y_down);
+  for (delta_c.y = top_left.y; -bot_right.y <= delta_c.y; --delta_c.y) {
+    for (delta_c.x = -top_left.x; delta_c.x <= bot_right.x; ++delta_c.x) {
       auto coord = center_cell + delta_c;
       auto cell_occ = map[coord];
       char map_cell_content;
@@ -28,7 +30,7 @@ void show_grid_map(const GridMap &map, const Point2D &center,
 }
 
 void show_grid_map(const GridMap &map, const Point2D &center,
-                   int d_x, int d_y) {
+                   double d_x, double d_y) {
   show_grid_map(map, center, d_x, d_x, d_y, d_y);
 }
 
