2fe7a4332e9985378fd2376f1c2a45079cff7ed5
Dmitry Kartashov <mapseamoff@mail.ru>
2018-05-29 23:33:30 +0300

    [Pkg] minor fixes

--- src/core/maps/occupancy_map.h
+++ src/core/maps/occupancy_map.h
@@ -3,12 +3,16 @@
 
 #include "../states/state_data.h"
 
+/*! \brief A base class for occupancy maps
+ *  \tparam AreaId a map area identifier (e.g. coordinate: DiscretePoint2D)
+ *  \tparam OccupancyInfo a type of occupancy information stored in a map
+ */
 template <typename AreaId, typename OccupancyInfo>
 class OccupancyMap {
   // TODO: [API Clean Up] Rename, e.g. add_observation.
-  /* Updates area with a given observation. */
+  /*! \brief Updates area with a given observation. */
   virtual void update(const AreaId &, const AreaOccupancyObservation &) = 0;
-  /* Returns known information about the occupancy of a given area. */
+  /*! \brief Returns known information about the occupancy of a given area. */
   virtual OccupancyInfo occupancy(const AreaId &) const = 0;
 };
 
